a24cf5a5ee45a0a8a8832ba1cb82a505df5416e6
Derin Joseph <derinbino@gmail.com>
2020-06-25 15:53:12 -0500

    added crossOrigin

--- src/main/java/com/dj/restapi/rest/ApiController.java
+++ src/main/java/com/dj/restapi/rest/ApiController.java
@@ -1,5 +1,6 @@
 package com.dj.restapi.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
